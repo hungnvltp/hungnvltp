8128502b46c00635c224199a548afb3d612d9846
hungnvltp <67050196+hungnvltp@users.noreply.github.com>
2020-10-09 15:55:06 +0700

    ddd

---
+++ src/component/TitleToDo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class TitleToDo extends React.Component {
+    render() {
+        return (
+            <p>Danh sach nhan vien</p>
+        )
+    }
+}
+
+export default TitleToDo;
